c542442ee5f82c59b5bc2c8f92801afede9da276
unknown <nafismahmudayon@gmail.com>
2024-09-07 09:56:20 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import type { Viewport } from "next";
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#f1f5f9' },
-    { media: '(prefers-color-scheme: dark)', color: '#0f172a' },
+    { media: '(prefers-color-scheme: light)', color: '#f1f6fb' },
+    { media: '(prefers-color-scheme: dark)', color: '#262626' },
   ],
 }
 
